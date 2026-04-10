7730eca57a897598d7350ab1baeed723fc0598e6
Skyerp <skyerp.skylabtech@gmail.com>
2026-04-10 14:54:48 +0530

    Revert "by vinay kam kiya gya"
    This reverts commit 38554dda00b454ec71c81dfd4a8a0818dc00978b.

--- portfolio.php
+++ portfolio.php
@@ -1,5 +1,23 @@
 <?php include 'header.php'; ?>
             
+            <!-- Page Header Start -->
+            <div class="page-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2>Case Studies</h2>
+                        </div>
+                        <div class="col-12">
+                            <a href="">Home</a>
+                            <a href="">Case Studies</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Page Header End -->
+
+
+            <!-- Portfolio Start -->
             <div class="portfolio">
                 <div class="container">
                     <div class="section-header">
